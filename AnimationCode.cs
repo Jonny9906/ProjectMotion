2b07d2a82a40662274ffbcec62b6abe1619e2265
Jonny9906 <108834152+Jonny9906@users.noreply.github.com>
2023-11-22 15:32:29 -0800

    Update AnimationCode.cs

--- AnimationCode.cs
+++ AnimationCode.cs
@@ -11,29 +11,34 @@ public class AnimationCode : MonoBehaviour
     List<string> lines;
     List<string> lines2;
     int counter = 1;
-    // Start is called before the first frame update
+    
     void Start()
     {
+        // Access the files contaning coordinates from the video(s)
         lines = System.IO.File.ReadLines("Assets/JonnySwing.txt").ToList();
         lines2 = System.IO.File.ReadLines("Assets/SwingFile.txt").ToList();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        print(lines2);
+        // Splits coordniates into lists
         string[] points = lines[counter].Split(',');
         string[] points2 = lines2[counter].Split(',');
+
+        // Iterates 16 times, each iteration is for an individual sphere
         for (int i = 0; i <= 16; i++)
         {
+            // Assigns x, y, and z values the 
             float x = float.Parse(points[0 + (i * 2)]) / 100;
             float y = -float.Parse(points[1 + (i * 2)]) / 100;
             float z = -float.Parse(points2[0 + (i * 2)]) / 100;
-            Body[i].transform.localPosition = new Vector3(x, y, z);
 
+            //Changes location of the indivdual sphere
+            Body[i].transform.localPosition = new Vector3(x, y, z);
         }
 
-
+        // Time inbetween frames
         counter += 1;
         if (counter == lines.Count) { counter = 0; }
         Thread.Sleep(30);
